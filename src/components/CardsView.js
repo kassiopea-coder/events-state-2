c34e3ba27da58b5c2f1fc76b6e63f500582b1b50
Nadezhda <7_nadezhda@mail.ru>
2022-02-27 18:15:13 +0400

    fix cardsview

--- src/components/CardsView.js
+++ src/components/CardsView.js
@@ -3,13 +3,15 @@ import ShopCard from './ShopCard';
 import PropTypes from 'prop-types';
 
 export default function CardsView({ cards }) {
+  if (cards.length === 0) return null;
+
   return (
     <div className="CardsView">
-      {cards.length !== 0 ? cards.map((card) => {
+      {cards.map((card) => {
         return (
           <ShopCard card={card} key={card.name + card.color} />
         );
-      }) : alert('null')}
+      })}
     </div>
   );
 }
